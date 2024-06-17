13a7b18254d312d3cbacb4f78522f52060bfa3eb
ZanattaFleek <zanattafleek@gmail.com>
2024-06-17 19:40:39 -0300

    CodigoValidacao

---
+++ Html/conceitos/validacoes.js
@@ -0,0 +1,37 @@
+function validarVazio(idTxtElemento, idLblErro, mensagemErro) {
+  let retorno = true;
+
+  const txtCampo = document.getElementById(idTxtElemento);
+  const lblErro = document.getElementById(idLblErro);
+
+  if (txtCampo.value.length === 0) {
+    retorno = false;
+  }
+
+  if (retorno) {
+    lblErro.innerHTML = "";
+  } else {
+    lblErro.innerHTML = mensagemErro;
+  }
+
+  return retorno;
+}
+
+function validarSomenteNumeros(idTxtElemento, idLblErro, mensagemErro) {
+  let retorno = true;
+
+  const txtCampo = document.getElementById(idTxtElemento);
+  const lblErro = document.getElementById(idLblErro);
+
+  if (txtCampo.value.length === 0) {
+    retorno = false;
+  }
+
+  if (retorno) {
+    lblErro.innerHTML = "";
+  } else {
+    lblErro.innerHTML = mensagemErro;
+  }
+
+  return retorno;
+}
